e0e8ca316e4b45d04ad1c0c9721d5e21f2a1ab59
HrushikeshSeth <69381701+HrushikeshSeth@users.noreply.github.com>
2021-07-19 23:42:53 +0530

    WeShare Website

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Welcome  To WeShare - Sign up, Log in </title>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+	</head>
+
+<body>
+
+	<div id="header">
+		<div class="head-view">
+			<ul>
+				<li><b class="logo">WeShare</b></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li><a href="signin.php" title="Log in"><button class="btn-sign-in" value="Sign in">Log in</button></a></li>
+				<li><a href="signup.php" title="Sign up"><button class="btn-sign-up" value="Sign up">Sign up</button></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="container">
+		<div class="image-display">
+				<!-- <img src="image/social.jpg" class="img-style" /> -->
+		</div>
+	</div>
+
+</body>
+
+</html>
